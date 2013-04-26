5e30aab8e125967f4c79753c7ea76ff3073960f9
Jason Owen <jason.a.owen@gmail.com>
2013-04-25 18:27:52 -0700

    parse multiple links in a line; use more idiomatic perl

--- youtube.pl
+++ youtube.pl
@@ -13,49 +13,47 @@ $VERSION = '0.01';
 );
 
 sub message {
-  my ($server, $message, $nick, $mask, $target) = @_;
+  my ($server, $_, $nick, $mask, $target) = @_;
   return unless $server;
 
   #$server->print($target, "server: $server, data: $data, nick: $nick, mask: $mask, target: $target", MSGLEVEL_CRAP);
 
-  if (contains_youtube_link($message)) {
-    my $video_id = get_video_id($message);
-    $server->print($target, youtube_details($video_id), MSGLEVEL_CRAP);
+  foreach my $word (split) {
+    if (contains_youtube_link($word)) {
+      my $video_id = get_video_id($word);
+      $server->print($target, youtube_details($video_id), MSGLEVEL_CRAP);
+    }
   }
 }
 
 sub contains_youtube_link {
-  my ($message) = @_;
-  return (contains_youtube_fulllink($message) or contains_youtube_shortlink($message))
+  ($_) = @_;
+  return (contains_youtube_fulllink() or contains_youtube_shortlink())
 }
 
 sub contains_youtube_fulllink {
-  ($_) = @_;
   return /youtube\.com\/watch\?.+/;
 }
 
 sub contains_youtube_shortlink {
-  ($_) = @_;
   return /youtu\.be\/.+/;
 }
 
 sub get_video_id {
-  my ($message) = @_;
-  if (contains_youtube_shortlink($message)) {
-    return get_video_id_from_shortlink($message);
+  ($_) = @_;
+  if (contains_youtube_shortlink()) {
+    return get_video_id_from_shortlink();
   } else {
-    return get_video_id_from_fulllink($message);
+    return get_video_id_from_fulllink();
   }
 }
 
 sub get_video_id_from_shortlink {
-  ($_) = @_;
   /youtu\.be\/([A-Za-z0-9_-]{11})/;
   return $1;
 }
 
 sub get_video_id_from_fulllink {
-  ($_) = @_;
   /v=([A-Za-z0-9_-]{11})/;
   return $1;
 }
